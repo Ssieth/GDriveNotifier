1e1ed87d8b1482599fe58100f619bac8497fb2aa
Ssieth <github@ssieth.co.uk>
2020-07-22 20:34:44 +0100

    Update version number to 1.8.0

--- GDriveNotifier/Properties/Settings.Designer.cs
+++ GDriveNotifier/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace GDriveNotifier.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.4.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -73,7 +73,7 @@ namespace GDriveNotifier.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("1.7.0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("1.8.0")]
         public string AppVer {
             get {
                 return ((string)(this["AppVer"]));
